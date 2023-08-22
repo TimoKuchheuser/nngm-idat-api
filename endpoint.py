ac01d5aeec75288b719f0123ad6226d821befa7e
Timo Kuchheuser <timo.kuchheuser@uk-koeln.de>
2023-08-22 10:37:49 +0200

    Copied the XSD into the file for usage with PyInstaller.

--- endpoint.py
+++ endpoint.py
@@ -10,18 +10,103 @@ from nacl.public import PublicKey, SealedBox
 from nacl.encoding import HexEncoder, Base64Encoder
 
 """
+As the system will be used with pyinstaller, having the xsd as a string in the
+file makes life easier.
+
+- The start of the string CAN NOT have any whitespace
+- The string MUST be passed as bytes
+"""
+xsd_schema: bytes = b"""<?xml version="1.0" encoding="UTF-8"?>
+<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema" elementFormDefault="qualified">
+  <xs:element name="patient">
+    <xs:complexType mixed="true">
+      <xs:sequence>
+        <xs:element ref="vorname"/>
+        <xs:element ref="nachname"/>
+        <xs:element ref="geburtstag"/>
+        <xs:element ref="geburtsmonat"/>
+        <xs:element ref="geburtsjahr"/>
+        <xs:element ref="versicherungsnummer"/>
+        <xs:element ref="adresse.plz"/>
+        <xs:element ref="adresse.stadt"/>
+        <xs:element ref="adresse.strasse"/>
+      </xs:sequence>
+    </xs:complexType>
+  </xs:element>
+  <xs:element name="vorname">
+    <xs:simpleType>
+      <xs:restriction base="xs:string">
+        <xs:minLength value="1"/>
+      </xs:restriction>
+    </xs:simpleType>
+  </xs:element>
+  <xs:element name="nachname">
+    <xs:simpleType>
+      <xs:restriction base="xs:string">
+        <xs:minLength value="1"/>
+      </xs:restriction>
+    </xs:simpleType>
+  </xs:element>
+  <xs:element name="geburtstag">
+    <xs:simpleType>
+      <xs:restriction base="xs:integer">
+        <xs:pattern value="[0-3][0-9]"/>
+        <xs:maxInclusive value="31"/>
+        <xs:minInclusive value="1"/>
+      </xs:restriction>
+    </xs:simpleType>
+  </xs:element>
+  <xs:element name="geburtsmonat">
+    <xs:simpleType>
+      <xs:restriction base="xs:integer">
+        <xs:minInclusive value="1"/>
+        <xs:maxInclusive value="12"/>
+        <xs:pattern value="[0-1][0-9]"/>
+      </xs:restriction>
+    </xs:simpleType>
+  </xs:element>
+  <xs:element name="geburtsjahr">
+    <xs:simpleType>
+      <xs:restriction base="xs:integer">
+        <xs:minInclusive value="1900"/>
+        <xs:maxInclusive value="2045"/>
+        <xs:pattern value="[1-2][0-9][0-9][0-9]"/>
+      </xs:restriction>
+    </xs:simpleType>
+  </xs:element>
+  <xs:element name="versicherungsnummer">
+    <xs:simpleType>
+      <xs:restriction base="xs:string">
+        <xs:minLength value="1"/>
+        <xs:maxLength value="32"/>
+      </xs:restriction>
+    </xs:simpleType>
+  </xs:element>
+  <xs:element name="adresse.plz">
+    <xs:simpleType>
+      <xs:restriction base="xs:string"> </xs:restriction>
+    </xs:simpleType>
+  </xs:element>
+  <xs:element name="adresse.stadt">
+    <xs:simpleType>
+      <xs:restriction base="xs:string"> </xs:restriction>
+    </xs:simpleType>
+  </xs:element>
+  <xs:element name="adresse.strasse">
+    <xs:simpleType>
+      <xs:restriction base="xs:string"> </xs:restriction>
+    </xs:simpleType>
+  </xs:element>
+</xs:schema>
+"""
+
+"""
 Setup 
 """
-schema_xml: etree.Element = None
 xml_schema: etree.XMLSchema = None
-try:
-    schema_xml = etree.parse("schema.xsd")
-except FileNotFoundError as err:  # Fatal
-    print("ERROR - XSD Schema file not found - Exiting")
-    sys.exit(1)
 
 try:
-    xml_schema = etree.XMLSchema(schema_xml)
+    xml_schema = etree.fromstring(xsd_schema)
 except etree.XMLSchemaError as err:  # Fatal
     print(f"ERROR - XSD Schema error - Exiting: {err}")
     sys.exit(1)
